011d78aed446a511bac85ea20b727cb6cf3924b8
JeterChan <clocklife0226@gmail.com>
2025-05-11 20:10:48 +0200

    feat: set express server

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const orderRouter = require('./routes/order');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
+app.use('/', orderRouter);
+
+app.listen(3000, ()=> {
+    console.log('Server running on http://localhost:3000')
+})
